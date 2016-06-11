b2e70d96631466d243c54ccc0d92b98ba23a240a
Jose Coelho <jose.coelho@skybettingandgaming.com>
2016-06-11 19:50:46 +0100

    improved service

--- lib/chef/handler/resources/service.rb
+++ lib/chef/handler/resources/service.rb
@@ -5,14 +5,24 @@ class Chef
     class Service < Chef::Resource
       def to_serverspec
         ERB.new(
-          <<-EOT
+          <<-EOT,
 
   describe service('<%= name %>') do
+<%- if action.include? :enable -%>
     it { should be_enabled }
+<%- end -%>
+<%- if action.include? :disable -%>
+    it { should_not be_enabled }
+<%- end -%>
+<%- if action.include? :start -%>
     it { should be_running }
+<%- end -%>
+<%- if action.include? :stop -%>
+    it { should_not be_running }
+<%- end -%>
   end
 EOT
-        ).result(binding)
+          safe_level = nil, trim_mode = '-').result(binding)
       end
     end
   end
--- spec/resources/service_spec.rb
+++ spec/resources/service_spec.rb
@@ -3,9 +3,11 @@ require 'spec_helper'
 describe Chef::Resource::Service do
   before(:each) do
     @service = Chef::Resource::Service.new('something')
+    @service.action += [:enable, :start]
   end
 
   it 'generates serverspec definitions' do
     expect(@service.to_serverspec).to match(/should be_enabled/)
+    expect(@service.to_serverspec).to match(/should be_running/)
   end
 end
