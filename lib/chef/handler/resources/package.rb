1de3fdf68e8169621adb85dfce7f9e1e824a195e
José Coelho <jose.alberto.coelho@gmail.com>
2016-06-13 07:59:27 +0100

    package version may be an array (#4)

--- lib/chef/handler/resources/package.rb
+++ lib/chef/handler/resources/package.rb
@@ -2,12 +2,19 @@ class Chef
   class Resource
     class Package < Chef::Resource
       def to_serverspec
+        let = case version
+              when Array
+                version.first
+              else
+                version
+              end
+
         ERB.new(
           <<-EOT,
 
   describe package('#{package_name}') do
 <%- unless action.include? :remove -%>
-    it { should be_installed#{'.with_version(\'' + version + '\')' unless version.nil?} }
+    it { should be_installed#{'.with_version(\'' + let + '\')' unless let.nil?} }
 <%- else -%>
     it { should_not be_installed }
 <%- end -%>
